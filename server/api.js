265cc99d9dd287064e2d0487dc867bd0799b53c1
Mark Hario <hario.mark@gmail.com>
2019-01-20 14:57:09 -0700

    add API index, basic logging

---
+++ server/api.js
@@ -0,0 +1,19 @@
+'use strict'
+const router = require('express').Router()
+const volleyball = require('volleyball')
+
+router
+	.get('/heartbeat', (req, res) => res.send({ ok: true }))
+
+	// Send along any errors
+	.use((err, req, res, next) => {
+		console.log(err);
+		res.status(500).send(err);
+	})
+
+	.use('/', function (req, res) {
+		res.sendStatus(404)
+	})
+
+
+module.exports = router
---
+++ server/start.js
@@ -0,0 +1,24 @@
+'use strict'
+const express = require('express')
+const { resolve } = require('path')
+const app = express()
+
+const volleyball = require('volleyball')
+
+module.exports = app
+
+	.use(volleyball)	// basic color-coded logging
+
+	.use('/api', require('./api'))
+	
+	.get('/*', (req, res) => {
+		res.sendStatus(404)
+		// res.sendFile(resolve(__dirname, '..', 'public', 'index.html'))
+		// hove not built front-end yet; using dev server for now
+	})
+
+const server = app.listen(8080,
+	() => {
+		console.log('Listening on 8080')
+	}
+)
