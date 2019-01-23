b9f11a4fab3c579965d433252d0d6ad2a82ff689
Mark Hario <hario.mark@gmail.com>
2019-01-22 20:40:08 -0700

    use actual routing

---
+++ server/search.js
@@ -0,0 +1,25 @@
+'use strict'
+const router = require('express').Router()
+const axios = require('axios').create({
+	headers: {
+		Authorization: "Bearer ifmcHckXmEX3wR_GyngSNs27MYfuZX5e_fIUXxNgM14RE5s0LENxjVSewfVHrjwl5mqAKSKTJfpwt0TVC89u1MPGYSkGLrSu8CgbwEoxANKZa5Kg4VHTQ4dNgctEXHYx"
+	}
+})
+
+router.get('/', (req, res) => {
+	axios({
+		method: 'get',
+		url: 'https://api.yelp.com/v3/businesses/search',
+		params: {
+			term: req.query.query,
+			latitude: 37.786882,
+			longitude: -122.399972
+		}
+	}).then(results => {
+		res.send(results.data)
+	}).catch(error => {
+		console.error(error)
+	})
+})
+
+module.exports = router
