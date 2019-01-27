e678f184489d45d4d8f7d05bb4c1d44b7b8a8902
Mark Hario <hario.mark@gmail.com>
2019-01-26 17:22:58 -0700

    Add redux and  react-redux; single stateful component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import './App.css';
 
+import YelpTester from './components/YelpTester'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<div className="App">
+					<header className="App-header">
+						<YelpTester />
+					</header>
+				</div>
+			</Provider>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/YelpTester.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { setRestaurants } from '../reducers/';
+
+class YelpTester extends Component {
+
+	requestRestaurants = () => {
+		setTimeout(() => {
+			this.props.setTheRestaurants([
+				{ name: 'hot dogs' },
+				{ name: 'beer' }
+			])
+		}, 1000)
+	}
+
+	render() {
+		return (
+			<div>
+				testing Yelp lol
+				<button
+					onClick={this.requestRestaurants}>
+					Get stuff!	
+				</button>
+				<div className="menu" style={{color: 'white'}}>
+					{
+						this.props.restaurants &&
+						this.props.restaurants.map(restaurant => {
+							return (
+								<p
+									key={restaurant.name}
+								>{restaurant.name}</p>
+							)
+						})
+					}
+				</div>
+			</div>
+		)
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		restaurants: state.restaurants
+	}
+}
+
+const mapDispatchToProps = dispatch => ({
+	setTheRestaurants(rests) {
+		dispatch(setRestaurants(rests))
+	}
+})
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(YelpTester)
