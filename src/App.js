c111a21d106fac5d38deedad31fff62d92cac3c1
Mark Hario <hario.mark@gmail.com>
2019-01-26 18:03:38 -0700

    add node-sass, styles folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import './App.css';
+import './styles/index.scss';
 
 import YelpTester from './components/YelpTester'
 
